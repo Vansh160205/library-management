c3c14d59b00b9cf7bf9ab01e634d0b64e2de8ca7
vansh-vagadia <vanshvagadia1602@gmail.com>
2025-05-14 19:24:24 +0530

    environment variable changes

--- library_mgmt/settings.py
+++ library_mgmt/settings.py
@@ -25,7 +25,11 @@ load_dotenv()
 # Now you can access the environment variables
 # OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
+DB_NAME=os.getenv("DB_NAME")
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST=os.getenv("DB_HOST")
+DB_PORT=os.getenv("DB_PORT")
 
 
 # Quick-start development settings - unsuitable for production
